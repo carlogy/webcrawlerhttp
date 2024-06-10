302cd83eb8f077d50c1721e637c0cb2c388456c6
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-06-10 08:51:56 -0400

    Started defining tests for the getURLsFromHTML function.

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl.js");
+const { normalizeURL, getURLsFromHTML } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -33,5 +33,23 @@ test("normalizeURL capitals", () => {
   const input = "https://BLOG.boot.dev/path";
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("gethtmlString", () => {
+  const input =
+    '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a></body></html>';
+  const actual = getURLsFromHTML(input);
+  const expected =
+    '<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a></body></html>';
+  expect(actual).toEqual(expected);
+});
+
+test("gethtmlString relativeURL", () => {
+  const htmlString =
+    '<html><body><a href="/path"><span>Go to Boot.dev</span></a></body></html>';
+  const baseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(htmlString, baseURL);
+  const expected = ["https://blog.boot.dev/path"];
   expect(actual).toEqual(expected);
 });
