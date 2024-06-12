b970d496fce5b1a35e12146d6b9da77bfaafe981
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2024-06-12 08:15:17 -0400

    updated main function to check number of command-line-arguments passed when nodejs process is initiated.

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-console.log("hello world!");
+const { readline } = require("node:readline");
+
+const { stdin: input, stdout: output } = require("node:process");
+
+const { argv } = require("node:process");
+
+function main() {
+  if (argv.length > 3) {
+    console.log(
+      `Error the number of arguments passed is more than required to execute.`,
+    );
+  } else if (argv.length < 3) {
+    console.log(
+      `Error the number of arguments passed is less than required to execute.`,
+    );
+  } else {
+    try {
+      const baseURL = argv[2];
+      console.log(`Initiating crawler at ${baseURL.href}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    // argv.forEach((val, index) => {
+    //   console.log(`${ index }: ${ val }`);
+    // });
+  }
+}
+
+main();
